9beedf8177b99c8f889adc5e7974fb605854d3c9
haylalira <haylalira1231@gmail.com>
2023-04-11 23:21:17 +0000

    projeto

---
+++ src/pages/sucess.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Sucess() {
+  return (
+    <h1>Sucess</h1>
+         
+  )
+}
---
+++ src/styles/index.ts
@@ -0,0 +1,14 @@
+ 
+ import { createStitches } from '@stitches/react'
+ 
+ 
+const { config, createTheme,css,getCssText,globalCss,keyframes,styled,theme} = createStitches({
+
+  theme:{
+    colors:{
+      hayla:'#fffff'
+    }
+  }
+})
+
+export { getCssText }
